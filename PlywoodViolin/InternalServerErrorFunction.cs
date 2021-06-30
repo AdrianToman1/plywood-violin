365328d8df7708a1005dc34415549b2ae5d160d3
Adrian Toman <adrian@adriantoman.com>
2021-06-30 21:12:51 +1000

    Import functions from Watchers test site

---
+++ PlywoodViolin/InternalServerErrorFunction.cs
@@ -0,0 +1,20 @@
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace PlywoodViolin
+{
+    public static class InternalServerErrorFunction
+    {
+        [FunctionName("InternalServerErrorFunction")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "InternalServerError")] HttpRequest req,
+            ILogger log)
+        {
+            return new InternalServerErrorResult();
+        }
+    }
+}

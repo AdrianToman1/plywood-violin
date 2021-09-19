122a9c269eb40f21d643642808bba28a31e36ab9
Adrian Toman <adrian@adriantoman.com>
2021-09-19 20:56:40 +1000

    Extract WebJobsRouterExtensions into seperate file

--- src/PlywoodViolin/Startup.cs
+++ src/PlywoodViolin/Startup.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using PlywoodViolin;
@@ -42,38 +37,6 @@ namespace PlywoodViolin
         {
             builder.AddExtension<RoutePriorityExtensionConfigProvider>();
             return builder;
-        }
-    }
-
-    public static class IWebJobsRouterExtensions
-    {
-        public static List<Route> GetRoutes(this IWebJobsRouter router)
-        {
-            var type = typeof(WebJobsRouter);
-            var fields = type.GetRuntimeFields();
-            var field = fields.FirstOrDefault(f => f.Name == "_functionRoutes");
-            var functionRoutes = field.GetValue(router);
-            var routeCollection = (RouteCollection)functionRoutes;
-            var routes = GetRoutes(routeCollection);
-            return routes;
-        }
-
-        private static List<Route> GetRoutes(RouteCollection collection)
-        {
-            var routes = new List<Route>();
-            for (var i = 0; i < collection.Count; i++)
-            {
-                var nestedCollection = collection[i] as RouteCollection;
-                if (nestedCollection != null)
-                {
-                    routes.AddRange(GetRoutes(nestedCollection));
-                    continue;
-                }
-
-                routes.Add((Route)collection[i]);
-            }
-
-            return routes;
         }
     }
 }
---
+++ src/PlywoodViolin/WebJobsRouterExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace PlywoodViolin
+{
+    public static class WebJobsRouterExtensions
+    {
+        public static List<Route> GetRoutes(this IWebJobsRouter router)
+        {
+            var type = typeof(WebJobsRouter);
+            var fields = type.GetRuntimeFields();
+            var field = fields.FirstOrDefault(f => f.Name == "_functionRoutes");
+            var functionRoutes = field.GetValue(router);
+            var routeCollection = (RouteCollection)functionRoutes;
+            var routes = GetRoutes(routeCollection);
+            return routes;
+        }
+
+        private static List<Route> GetRoutes(RouteCollection collection)
+        {
+            var routes = new List<Route>();
+            for (var i = 0; i < collection.Count; i++)
+            {
+                var nestedCollection = collection[i] as RouteCollection;
+                if (nestedCollection != null)
+                {
+                    routes.AddRange(GetRoutes(nestedCollection));
+                    continue;
+                }
+
+                routes.Add((Route)collection[i]);
+            }
+
+            return routes;
+        }
+    }
+}
